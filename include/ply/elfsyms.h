4a068c2a6a04c1ed060a0d551c05c6301afd982f
Stefan Schake <stschake@gmail.com>
2023-09-13 02:02:33 +0200

    libply: add helper to read ELF symbol tables
    This can read either the symbol table (preferred if present) or
    the dynamic symbol table from ELF files with no external dependencies.
    Preparatory work for userspace probes where having some source of
    symbol table is required or users need to provide raw offsets.

---
+++ include/ply/elfsyms.h
@@ -0,0 +1,28 @@
+#ifndef __ELFSYMS_H
+#define __ELFSYMS_H
+
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct symbol {
+    const char *name;
+    intptr_t offset;
+    size_t size;
+};
+
+struct elfsyms {
+    int fd;
+    void *data;
+    off_t size;
+
+    struct symbol *symbols;
+    size_t nsymbols;
+};
+
+const struct symbol *elfsyms_lookup(struct elfsyms* es, const char *name);
+struct elfsyms *elfsyms_open(const char *file);
+void elfsyms_free(struct elfsyms* elf);
+
+#endif	/* __ELFSYMS_H */
---
+++ src/libply/aux/elfsyms.c
@@ -0,0 +1,148 @@
+#include <ply/elfsyms.h>
+#include <ply/ply.h>
+#include <ply/utils.h>
+
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+
+#include <elf.h>
+
+#if ULONG_MAX == 0xffffffff
+typedef Elf32_Ehdr Ehdr;
+typedef Elf32_Shdr Shdr;
+typedef Elf32_Sym Sym;
+#else
+typedef Elf64_Ehdr Ehdr;
+typedef Elf64_Shdr Shdr;
+typedef Elf64_Sym Sym;
+#endif
+
+static int symbol_cmp(const void *a, const void *b)
+{
+	const struct symbol *syma = a;
+    const struct symbol *symb = b;
+
+	return strcmp(syma->name, symb->name);
+}
+
+const struct symbol *elfsyms_lookup(struct elfsyms* es, const char *name)
+{
+    struct symbol key = { .name = name };
+
+    if (!es || !es->symbols || !es->nsymbols)
+        return NULL;
+    
+    return bsearch(&key, es->symbols, es->nsymbols, sizeof(struct symbol), symbol_cmp);
+}
+
+static int read_symbols_table(struct elfsyms *es, const Shdr *symtab,
+                const Shdr *strtab)
+{
+    const Sym *syms = es->data + symtab->sh_offset;
+    const char *strs = es->data + strtab->sh_offset;
+    const size_t nsyms = symtab->sh_size / sizeof(Sym);
+    size_t i;
+
+    es->symbols = xcalloc(nsyms, sizeof(struct symbol));
+    for (i = 0; i < nsyms; i++) {
+        const Sym *sym = syms + i;
+        struct symbol *res;
+        
+        if (!sym->st_name || !sym->st_value || !sym->st_size)
+            continue;
+        res = es->symbols + es->nsymbols++;
+        res->name = strs + sym->st_name;
+        res->offset = sym->st_value;
+        res->size = sym->st_size;
+    }
+
+    return 0;
+}
+
+static int read_symbols(struct elfsyms *es)
+{
+    const Ehdr *hdr = es->data;
+    int ret = 0;
+    const Shdr *dynsym = NULL, *dynstr = NULL;
+    const Shdr *symtab = NULL, *strtab = NULL;
+    const Shdr *shstrtab;
+    size_t i;
+
+    if (memcmp(hdr->e_ident, ELFMAG, SELFMAG))
+        return -EINVAL;
+    if (sizeof(Shdr) != hdr->e_shentsize)
+        return -EINVAL;
+
+    shstrtab = es->data + hdr->e_shoff + (hdr->e_shstrndx * hdr->e_shentsize);
+    for (i = 0; i < hdr->e_shnum; i++) {
+        const Shdr *shdr = es->data + hdr->e_shoff + (i * hdr->e_shentsize);
+
+        if (shdr->sh_type == SHT_DYNSYM)
+            dynsym = shdr;
+        else if (shdr->sh_type == SHT_SYMTAB)
+            symtab = shdr;
+        else if (shdr->sh_type == SHT_STRTAB) {
+            const char *sname = es->data + shstrtab->sh_offset + shdr->sh_name;
+            if (strcmp(sname, ".dynstr") == 0)
+                dynstr = shdr;
+            else if (strcmp(sname, ".strtab") == 0)
+                strtab = shdr;
+        }
+    }
+    
+    if (symtab && strtab)
+        ret = read_symbols_table(es, symtab, strtab);
+    else if (dynsym && dynstr)
+        ret = read_symbols_table(es, dynsym, dynstr);
+
+    if (!ret && es->nsymbols) {
+        qsort(es->symbols, es->nsymbols, sizeof(struct symbol), symbol_cmp);
+    }
+    return ret;
+}
+
+struct elfsyms *elfsyms_open(const char *file)
+{
+    struct elfsyms *es;
+    struct stat st;
+
+    _d("opening elf %s\n", file);
+    if (stat(file, &st))
+        return NULL;
+
+    es = xcalloc(1, sizeof(*es));
+    es->fd = open(file, O_RDONLY);
+    es->size = st.st_size;
+    if (es->fd < 0)
+        goto err;
+    es->data = mmap(NULL, es->size, PROT_READ, MAP_SHARED, es->fd, 0);
+    if (es->data == MAP_FAILED)
+        goto err;
+
+    if (read_symbols(es))
+        goto err;
+    return es;
+
+    err:
+    if (es->data)
+        munmap(es->data, es->size);
+    if (es->fd)
+        close(es->fd);
+    free(es);
+    return NULL;
+}
+
+void elfsyms_free(struct elfsyms* es)
+{
+    munmap(es->data, es->size);
+    close(es->fd);
+    free(es);
+}
